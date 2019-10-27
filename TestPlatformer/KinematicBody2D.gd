e655e577a47b7a2d074be9dd63ad11fd7832d3e8
James <jyee0012@gmail.com>
2019-10-27 15:05:25 -0600

    Added Collisions and ElementState
    isolated movement and collision into seperate functions

--- TestPlatformer/KinematicBody2D.gd
+++ TestPlatformer/KinematicBody2D.gd
@@ -1,14 +1,49 @@
 extends KinematicBody2D
 
 const UP = Vector2(0,-1)
-const GRAVITY = 20
+var grav = 20
 var speed = 100
-const JUMP_HEIGHT = 500
+var jumpHeight = 500
 var motion = Vector2()
 var jump_count = 1 
 
+enum ElementState{
+	None,
+	Fire,
+	Water,
+	Wind
+}
+var currentElement = ElementState.None
+
 func _physics_process(delta):
-	motion.y += GRAVITY
+	
+	_movement(delta)
+	_element_interaction()
+	
+	if Input.is_action_just_pressed("ui_reset"):
+		get_tree().reload_current_scene()
+
+func _element_interaction():
+	var get_col = null
+	if (get_slide_count() > 0):
+		get_col = get_slide_collision(get_slide_count()-1)
+		
+	if get_col != null:
+		if get_col.collider.is_in_group("fire"):
+			print("fire")
+			if (currentElement == ElementState.Fire):
+				get_col.collider.free()
+		elif get_col.collider.is_in_group("water"):
+			print("water")
+			if (currentElement == ElementState.Water):
+				get_col.collider.free()
+		elif get_col.collider.is_in_group("wind"):
+			print("wind")
+			if (currentElement == ElementState.Wind):
+				get_col.collider.free()
+
+func _movement(delta):
+	motion.y += grav
 	if Input.is_action_pressed("ui_left"):
 		motion.x = -speed
 		if is_on_floor():
@@ -24,21 +59,17 @@ func _physics_process(delta):
 	if motion.x == 0 && is_on_floor():
 		$AnimationPlayer.play('Idle')
 	if Input.is_action_just_pressed("ui_up") && jump_count > 0:
-		motion.y = -JUMP_HEIGHT
+		motion.y = -jumpHeight
 		$AnimationPlayer.play('Jump')
 		jump_count -= 1
 	if is_on_floor():
 		jump_count = 1
 	if Input.is_action_pressed('ui_up') && is_on_wall():
-		motion.y = -JUMP_HEIGHT
+		motion.y = -jumpHeight
 	if Input.is_action_pressed("ui_space"):
 		speed = 200
 	else:
 		speed = 100
-	if Input.is_action_just_pressed("ui_reset"):
-		get_tree().reload_current_scene()
-	if Input.is_action_pressed("ui_accept"):
-		#comment: try to find how to determine which collide i hit
-		#and if it is a obstacle then disable or hide the node
-		print(get_slide_collision(get_slide_count()-1).collider_shape)
+		
+		
 	motion = move_and_slide(motion, UP)
