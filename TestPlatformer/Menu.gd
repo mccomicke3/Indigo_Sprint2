2347a9831c698de368b6a1b1c48251320a238b38
James <jyee0012@gmail.com>
2019-10-31 21:28:58 -0600

    Added interactable menu

---
+++ TestPlatformer/Menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	visible = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		visible = !visible
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
+
+
+func _on_bFullscreen_toggled(button_pressed):
+	OS.set_window_fullscreen(button_pressed)
