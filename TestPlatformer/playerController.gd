26d518bfe5962c75c2a052040439ec18fcdad46d
James <jyee0012@gmail.com>
2019-11-01 16:32:09 -0600

    added more jumps when having wind element

--- TestPlatformer/playerController.gd
+++ TestPlatformer/playerController.gd
@@ -30,27 +30,29 @@ func _element_interaction():
 		
 	if get_col != null:
 		if get_col.collider.is_in_group("fire"):
-			print("fire")
 			if (currentElement == ElementState.Fire):
 				get_col.collider.free()
 		elif get_col.collider.is_in_group("water"):
-			print("water")
 			if (currentElement == ElementState.Water):
 				get_col.collider.free()
 		elif get_col.collider.is_in_group("wind"):
-			print("wind")
 			if (currentElement == ElementState.Wind):
 				get_col.collider.free()
 
 func _elementChange():
-	if Input.is_action_pressed("ui_4"):
+	if Input.is_action_just_pressed("ui_4"):
 		currentElement = ElementState.None
-	if Input.is_action_pressed("ui_1"):
+		print("noneState")
+	if Input.is_action_just_pressed("ui_1"):
 		currentElement = ElementState.Fire
-	if Input.is_action_pressed("ui_2"):
+		print("fireState")
+	if Input.is_action_just_pressed("ui_2"):
 		currentElement = ElementState.Water
-	if Input.is_action_pressed("ui_3"):
+		print("waterState")
+	if Input.is_action_just_pressed("ui_3"):
 		currentElement = ElementState.Wind
+		print("windState")
+		
 
 
 
@@ -74,12 +76,16 @@ func _movement(delta):
 		motion.x = 0
 	if motion.x == 0 && is_on_floor():
 		$AnimationPlayer.play('Idle')
-	if Input.is_action_just_pressed("ui_up") && jump_count > 0:
+	if Input.is_action_just_pressed("ui_up") && jump_count >= 0:
+		jump_count -= 1
 		motion.y = -jumpHeight
 		$AnimationPlayer.play('Jump')
-		jump_count -= 1
 	if is_on_floor():
-		jump_count = 1
+		#triple jump when having wind, double jump otherwise
+		if (currentElement == ElementState.Wind):
+			jump_count = 1
+		else:
+			jump_count = 0
 	if Input.is_action_pressed('ui_up') && is_on_wall():
 		motion.y = -jumpHeight
 	if Input.is_action_pressed("ui_space"):
